e4edaacd4feab4da921c71b4a7884325b8a13aea
Mousa Sabah <mosassbbah@gmail.com>
2023-09-25 01:09:48 +0300

    userAgent and chromeWebSecurity configuration

--- cypress.config.js
+++ cypress.config.js
@@ -10,6 +10,8 @@ module.exports = defineConfig({
     specPattern: '**/*.feature',
     baseUrl: 'https://www.propertyfinder.bh/',
     retries: 2,
+    chromeWebSecurity: false,
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
   },
 
   
